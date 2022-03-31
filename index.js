f26ce07c76415c17f5e00492146cab2e080789f6
dogerish <luca.filmstrip@gmail.com>
2022-03-31 16:48:01 -0400

    Evaluate paramters in URL to make testing easier

--- index.js
+++ index.js
@@ -24,3 +24,30 @@ function endgame()
 }
 
 $("wear-mask").onclick = function(e) { $("play-button").disabled = !this.checked; }
+
+// evaluates URL parameters from a string
+function evalParams(params)
+{
+	if (!params) return;
+	// parse the parmeters
+	let qstr = new URLSearchParams(params);
+	for (let param of qstr.entries())
+	{
+		switch (param[0])
+		{
+		// skip to a certain page
+		case "skipto":
+			$("title-screen").classList.add("hidden");
+			$(param[1]).classList.remove("hidden");
+			break;
+		// change the theme immediately
+		case "theme":
+			let s = $("theme-select");
+			s.value = param[1];
+			s.onchange();
+			break;
+		}
+	}
+}
+// evaluate parameters in the url
+evalParams(window.location.href.substr(window.location.href.indexOf('?') + 1));
